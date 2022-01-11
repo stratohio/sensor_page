ae025264bfceb0e7467bb4973c458f019e8e86d5
David Snyder <96398345+stratohio@users.noreply.github.com>
2022-01-10 19:26:33 -0500

    Add files via upload
    discrepancies were due to truncating relative humidity

--- src/test_dp.py
+++ src/test_dp.py
@@ -6,7 +6,7 @@ import math
 def dp1( t,h):
 	lamda=5390.0
 	K0 = 273.15
-	dp = lamda*(t+K0) / (lamda - ((t+K0) * math.log(h/100.0)))
+	dp = lamda*(t+K0) / (lamda - ((t+K0) * math.log(h)))
 	
 	return dp-K0
 
@@ -17,13 +17,14 @@ def dp1( t,h):
 def dp2(t,h):
 	beta = 17.625
 	lamda = 243.04
-	dp1 = lamda * (math.log(h/100.0)+((beta*t)/(lamda+t))) 
-	dp = dp1/(beta - (math.log(h/100.0)+ ((beta*t)/(lamda+t))))
+	dp1 = lamda * (math.log(h)+((beta*t)/(lamda+t))) 
+	dp = dp1/(beta - (math.log(h)+ ((beta*t)/(lamda+t))))
 	return dp
 
 temp = [30.0,25.0,20.0,15.0,10.0,0.0]
-hum = [100.0,75.0,55.0,40.0,29.0,14.0]
+hum = [42.5,31.7,23.4,17.1,12.3,6.1]
 
 for i in range(0,6):
-	print (i, temp[i], hum[i], dp1(30.0,hum[i]),dp2(30.0,hum[i]),math.log(hum[i]/100.0))
+	print (i, temp[i], hum[i]/hum[0], dp1(30.0,hum[i]/hum[0]),
+	  dp2(30.0,hum[i]/hum[0]),math.log(hum[i]/hum[0]))
 
